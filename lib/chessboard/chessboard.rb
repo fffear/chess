78b4d1e35344cff2f028b676b27318d2e2222b88
Kevin Vuong <c_k.k@hotmail.com>
2019-04-19 22:04:49 +1000

    add 'create_chessboard' to 'chessboard' class

---
+++ lib/chessboard/chessboard.rb
@@ -0,0 +1,27 @@
+# This is the class for the entire chessboard
+$LOAD_PATH << "#{File.expand_path('../../chessboard', __FILE__)}"
+require 'tile.rb'
+
+class Chessboard
+  attr_accessor :chessboard
+
+  def initialize
+    @chessboard = create_chessboard
+  end
+
+  private
+  def create_row(row_num)
+    row = []
+    ("a".."h").each { |l| row << Tile.new([l, row_num], " ") }
+    row
+  end
+
+  def create_chessboard
+    chessboard = []
+    (1..8).each { |n| chessboard += create_row(n) }
+    chessboard
+  end
+end
+
+# test = Chessboard.new
+# p test.chessboard
